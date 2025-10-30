d438a3cc5be50d964e597880676bc2a66f564fec
JFresnedaFuentespina <jfresnedafuentespina@gmail.com>
2025-10-30 19:58:48 +0100

    Asset de hogueras

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject roomPrefab;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

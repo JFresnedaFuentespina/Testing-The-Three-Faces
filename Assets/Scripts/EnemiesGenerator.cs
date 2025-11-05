0fd2cff36b31083c73a0bd2371a7a00dd25e32da
JFresnedaFuentespina <jfresnedafuentespina@gmail.com>
2025-11-05 10:12:27 +0100

    cambios ligeros en LevelGenerator.cs EnemiesGenerator INACABADO

---
+++ Assets/Scripts/EnemiesGenerator.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesGenerator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject enemyType1Prefab;
+    private int maxEnemies = 3;
+    private int minEnemies = 1;
+
+    void Start()
+    {
+
+    }
+
+    public void generateEnemiesInRoom()
+    {
+        
+    }
+
+}

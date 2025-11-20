f31e84890812fb500bee90f24dc6e07a7014e287
JFresnedaFuentespina <jfresnedafuentespina@gmail.com>
2025-11-20 15:40:52 +0100

    Generación de items en la habitación del tesoro ACABADO

--- Assets/Scripts/ItemGenerator.cs
+++ Assets/Scripts/ItemGenerator.cs
@@ -1,16 +1,15 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemGenerator : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public List<GameObject> items;
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        int random = Random.Range(0, items.Count);
+        GameObject chosen = items[random];
+        Vector3 spawnPoint = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
+        Instantiate(chosen, spawnPoint, Quaternion.identity);
     }
 }

709443a4780bce5d4f2ee06c4bcd677361170d81
JFresnedaFuentespina <jfresnedafuentespina@gmail.com>
2025-12-02 12:07:30 +0100

    healthbar enemigos

--- Assets/Scripts/EnemyLife.cs
+++ Assets/Scripts/EnemyLife.cs
@@ -1,24 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyLife : MonoBehaviour
 {
     // Start is called before the first frame update
     public float totalHp = 10f;
+    public float currentHp;
     private bool isAlive = true;
+    public GameObject healthBar;
+    private Image fillImage;
 
+    void Start()
+    {
+        currentHp = totalHp;
+        if (healthBar != null)
+        {
+            Transform fillTransform = healthBar.transform.Find("Canvas/Fill");
+            if (fillTransform != null)
+                fillImage = fillTransform.GetComponent<Image>();
+        }
+    }
     public void Damage(float hit)
     {
-        totalHp -= hit;
+        if (!isAlive) return;
+
+        currentHp -= hit;
+        currentHp = Mathf.Clamp(currentHp, 0f, totalHp);
+
+        UpdateHealthBar();
+        UpdateIsAlive();
     }
 
     public void UpdateIsAlive()
     {
-        if (totalHp <= 0)
+        if (currentHp <= 0 && isAlive)
         {
-            Debug.Log($"{gameObject.name} murió");
-            totalHp = 0;
             isAlive = false;
             Die();
         }
@@ -29,8 +47,17 @@ public class EnemyLife : MonoBehaviour
         return isAlive;
     }
 
+    void UpdateHealthBar()
+    {
+        if (fillImage != null)
+        {
+            fillImage.fillAmount = currentHp / totalHp;
+        }
+    }
+
     public void Die()
     {
+        Debug.Log($"{gameObject.name} murió");
         Destroy(gameObject);
     }
 }

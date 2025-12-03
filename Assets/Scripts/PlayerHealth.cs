641463edd9dec1ad714c00e1cb2f806f01da81ce
JFresnedaFuentespina <jfresnedafuentespina@gmail.com>
2025-12-03 10:00:46 +0100

    colisión con el fantasma

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -88,9 +88,8 @@ public class PlayerHealth : MonoBehaviour
     void EnableDeath() => canDie = true;
     private void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.CompareTag("Enemy_Zombie") || other.gameObject.CompareTag("BossCara")|| other.gameObject.CompareTag("EnemyProjectile"))
+        if (other.gameObject.CompareTag("Enemy_Zombie") || other.gameObject.CompareTag("Enemy_Ghost") || other.gameObject.CompareTag("BossCara")|| other.gameObject.CompareTag("EnemyProjectile"))
         {
-            Debug.Log("Player hit by " + other.gameObject.name);
             healthPoints -= 0.5f;
         }
         // Limita el valor antes de actualizar HUD

acd7afbd69febe086d049b0007864bc637182d37
JFresnedaFuentespina <jfresnedafuentespina@gmail.com>
2026-01-26 10:58:41 +0100

    Configuración audio (General)

--- Assets/Scripts/HUDScripts/OptionsAudioPercentatge.cs
+++ Assets/Scripts/HUDScripts/OptionsAudioPercentatge.cs
@@ -1,15 +1,52 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class OptionsAudioPercentatge : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public GameObject sliderGO;
-    private Slider slider;
+    [Header("UI References")]
+    public Slider audioSlider; // Asignar en el inspector
+    public TextMeshProUGUI audioPercentatgeTxt;
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        if (audioSlider != null)
+        {
+            // Configurar listener para cambios en el slider
+            audioSlider.onValueChanged.AddListener(OnSliderValueChanged);
+
+            // Inicializar el texto con el valor actual
+            UpdateAudioText(audioSlider.value);
+            UpdateAllAudioSourcesVolume(audioSlider.value);
+        }
+        else
+        {
+            Debug.LogError("Audio Slider no asignado en OptionsAudioPercentatge!");
+        }
+    }
+
+    private void OnSliderValueChanged(float value)
+    {
+        UpdateAudioText(value);
+        UpdateAllAudioSourcesVolume(value);
+    }
+
+    private void UpdateAudioText(float value)
+    {
+        int percentage = Mathf.RoundToInt(value * 100);
+        if (audioPercentatgeTxt != null)
+            audioPercentatgeTxt.text = percentage + "%";
+    }
+
+    private void UpdateAllAudioSourcesVolume(float value)
+    {
+        AudioSource[] sources = GameObject.FindObjectsByType<AudioSource>(FindObjectsSortMode.None);
+        foreach (var audio in sources)
+        {
+            if (audio != null)
+                audio.volume = value;
+        }
+
+        AudioListener.volume = value;
     }
 }

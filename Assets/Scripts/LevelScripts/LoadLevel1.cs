058bed854e5f5b8dfdaa7620ce9dec08f63500ef
JFresnedaFuentespina <jfresnedafuentespina@gmail.com>
2026-01-27 12:35:40 +0100

    loading canvas

--- Assets/Scripts/LevelScripts/LoadLevel1.cs
+++ Assets/Scripts/LevelScripts/LoadLevel1.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -10,9 +11,17 @@ public class LoadLevel1 : MonoBehaviour
     public VideoPlayer videoPlayer;
     public VideoClip loreVideo;
     public Button playButton;
+    public GameObject loadingPanel;
+    public GameObject fadePanel;
+    public bool isLoading = false;
     void Start()
     {
-
+        GameObject loadingCanvas = GameObject.Find("LoadingCanvas");
+        if (loadingCanvas != null)
+        {
+            loadingPanel = loadingCanvas.transform.Find("LoadingPanel")?.gameObject;
+            fadePanel = loadingCanvas.transform.Find("Fade")?.gameObject;
+        }
         playButton.onClick.AddListener(ShowLoreVideo);
         videoPlayer.loopPointReached += OnVideoEnd;
         if (loreVideo != null)
@@ -22,6 +31,14 @@ public class LoadLevel1 : MonoBehaviour
         else
         {
             videoPlayer.clip = null;
+        }
+    }
+
+    void Update()
+    {
+        if (videoPlayer.isPlaying && (Input.GetKeyDown(KeyCode.Space)))
+        {
+            SkipVideo();
         }
     }
 
@@ -49,8 +66,30 @@ public class LoadLevel1 : MonoBehaviour
         }
         SceneManager.LoadScene("Level1Scene");
     }
+
+    public void SkipVideo()
+    {
+        // StartCoroutine(FadeAndLoad());
+        CargarNivel1();
+    }
+
+    // private IEnumerator FadeAndLoad()
+    // {
+    //     // Activar panel y hacer fade in
+    //     // fadePanel.gameObject.SetActive(true);
+    //     // float duration = 0.3f;
+    //     // for (float t = 0; t < duration; t += Time.deltaTime)
+    //     // {
+    //     //     fadePanel.GetComponentalpha = t / duration;
+    //     //     yield return null;
+    //     // }
+    //     // fadePanel.alpha = 1;
+    //     // Ahora cargar la escena
+    //     CargarNivel1();
+    // }
     private void OnVideoEnd(VideoPlayer vp)
     {
         CargarNivel1();
     }
+
 }

25561c30bef4d282403c8e64be323af0fa38bbb9
JFresnedaFuentespina <jfresnedafuentespina@gmail.com>
2026-02-10 11:13:29 +0100

    good ending escena lista

--- Assets/Scripts/PlayerScripts/PlayerBehaviour3rdPersonCameraWithMouse.cs
+++ Assets/Scripts/PlayerScripts/PlayerBehaviour3rdPersonCameraWithMouse.cs
@@ -33,12 +33,13 @@ public class PlayerBehaviour3rdPersonCameraWithMouse : MonoBehaviour
 
         rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
 
-        // SPEED para Blend Tree (0–5)
-        float inputMagnitude = new Vector2(inputH, inputV).magnitude;
-        float speed = inputMagnitude * movementSpeed;
-
-        animatorEsqueleto.SetFloat("Speed", speed);
+        // Parámetros para Blend Tree
+        // Velocidad vertical: adelante (+) o atrás (-)
+        animatorEsqueleto.SetFloat("Vertical", inputV * movementSpeed);
+        // Velocidad horizontal: izquierda/derecha si quieres strafing
+        animatorEsqueleto.SetFloat("Horizontal", inputH * movementSpeed);
     }
+
 
 
 }

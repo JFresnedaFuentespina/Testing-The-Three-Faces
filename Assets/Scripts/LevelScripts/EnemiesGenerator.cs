7e86e37d995dee867a9bd8124a259d06eecbf3e6
JFresnedaFuentespina <jfresnedafuentespina@gmail.com>
2026-02-19 11:59:58 +0100

    desactivar script tester niveles y reducir daño para hacer build

--- Assets/Scripts/LevelScripts/EnemiesGenerator.cs
+++ Assets/Scripts/LevelScripts/EnemiesGenerator.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine.SceneManagement;
-using Codice.Client.Common;
 // using System;
 
 public class EnemiesGenerator : MonoBehaviour

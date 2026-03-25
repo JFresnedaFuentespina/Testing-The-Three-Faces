77c82d8b2db150fb78ad9d122bc506381d2362de
JFresnedaFuentespina <jfresnedafuentespina@gmail.com>
2026-03-25 09:31:22 +0100

    mensaje mostrar mapa

--- Assets/Scripts/HUDScripts/ShowMinimap.cs
+++ Assets/Scripts/HUDScripts/ShowMinimap.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class ShowMinimap : MonoBehaviour
 {
     public GameObject minimap;
+    public GameObject message;
     public bool showMinimap = false;
 
     // Update is called once per frame
@@ -12,11 +13,13 @@ public class ShowMinimap : MonoBehaviour
         {
             showMinimap = true;
             minimap.SetActive(showMinimap);
+            message.SetActive(!showMinimap);
         }
         else if (Input.GetKeyUp(KeyCode.Tab))
         {
             showMinimap = false;
             minimap.SetActive(showMinimap);
+            message.SetActive(!showMinimap);
         }
     }
 }
